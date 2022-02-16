8d02d0529c1664f522f24c1680cd49bf6dca545a
Chai Saetern <chaisaetern530@gmail.com>
2022-02-15 19:59:48 -0800

    update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const askBtn = document.getElementById("ask-btn");
+const eightballImg = document.getElementById("eightball-img");
+const questionInput = document.getElementById("question");
+
+// onClick function for an answer
+const rndImg = () => {
+    let rng = Math.floor(Math.random() * 20 + 1)
+    if (questionInput.value === "") {
+        alert("Sorry, you did not ask a question. Ask me anything!");
+        return;
+    } else {
+        eightballImg.src = `../magic8ball/img/magic8ball_${rng}.png`
+    }
+}
