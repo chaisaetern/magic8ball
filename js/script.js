5a3147b51d5a76b8e07af843a379bd7c106e804b
Chai Saetern <chaisaetern530@gmail.com>
2022-02-18 10:07:45 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const questionInput = document.getElementById("question");
 // onClick function for an answerx
 const rndImg = () => {
     let rng = Math.floor(Math.random() * 20 + 1);
-    questionInput.value === "" ? 
+    questionInput.value === "" ?
         alert("Sorry, you did not ask a question. Ask me anything!") : 
         eightballImg.src = `../magic8ball/img/magic8ball_${rng}.png`;
 }
